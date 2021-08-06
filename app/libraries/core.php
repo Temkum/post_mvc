1a25122fe0ed0a6f1941483da899c4c397c7c84e
Kum Jude Tem <judekum14@gmail.com>
2021-08-06 20:42:19 +0100

    Load controller from Url

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -14,11 +14,34 @@ class Core
   public function __construct()
   {
     # code...
-    $this->getUrl();
+    // print_r($this->getUrl());
+
+    $url = $this->getUrl();
+
+    // look in controllers for first value
+    if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+      // it exists, then set as current controller
+      $this->currentController = ucwords($url[0]);
+
+      //unset 0 index
+      unset($url[0]);
+    }
+
+    // require the controller
+    require_once '../app/controllers/' . $this->currentController . '.php';
+
+    // instantiate controller class
+    $this->currentController = new $this->currentController;
   }
 
   public function getUrl()
   {
-    echo $_GET['url'];
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/'); //remove the end slash
+      $url = filter_var($url, FILTER_SANITIZE_URL); //remove unwanted chars
+      $url = explode('/', $url); // convert to an array
+
+      return $url;
+    }
   }
 }
