51484c98d43b16b9e72dadba23aec85404c214fa
Kum Jude Tem <judekum14@gmail.com>
2021-08-07 08:36:03 +0100

    Made cleanup to the MVC framework

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,13 +4,11 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel = $this->model('Post');
     }
 
     public function index()
     {
-        $posts = $this->postModel->getPosts();
-        $data = ['title' => 'Welcome Home', 'posts' => $posts];
+        $data = ['title' => 'Welcome Home'];
 
         $this->view('index', $data);
     }
